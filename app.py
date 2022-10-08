817dc6eff1bf7aa05ac6a45cd24c799c20689fd8
Mohamed1702 <m1987.ahmed@gmail.com>
2022-10-08 16:36:30 +0200

    Hello

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import html # to connect with html pages
+import utils #represent preprocessing for features
+
+app = Flask(__name__) 
+@app.route('/') 
+def home(): 
+    return render_template('index.html') 
+
+@app.route('/predict/', methods = ['GET', 'POST']) 
+def predict():
+    if request.method == 'POST': 
+        Pregnancies	    = request.form.get('Pregnancies') 
+        Glucose	        = request.form.get('Glucose') 
+        BloodPressure	= request.form.get('BloodPressure')   
+        SkinThickness	= request.form.get('SkinThickness') 
+        Insulin	        = request.form.get('Insulin')
+        BMI	            = request.form.get('BMI') 
+        DiabetesPedigreeFunction	= request.form.get('DiabetesPedigreeFunction') 
+        Age	            = request.form.get('Age')
+    prediction = utils.preprocessdata(Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age)
+    return render_template('predict.html', prediction = prediction) 
+
+
+if __name__ == "__main__": 
+    app.run(debug=True) 
